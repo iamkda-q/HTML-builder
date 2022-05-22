015e4ae3c02f08399b3e31c9f31e5003325cc4fe
KDA <iamkda@yandex.ru>
2022-05-23 02:15:59 +0300

    feat: solve 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,127 +1,60 @@
 const fsPromises = require('fs/promises');
-const createReadStream = require('fs').createReadStream;
 const path = require('path');
-const createInterface = require('readline').createInterface;
-const componentExp = /\{\{[a-zA-z0-9]+\}\}/;
-// const { stdout } = require('process');
 const projectPath = path.join(__dirname, 'project-dist');
-const htmlPath = path.join(projectPath, 'index.html');
-// const components = path.join(__dirname, 'components');
-// const o = {};
-// fsPromises.readdir(path.join(__dirname, 'components'), {withFileTypes: true})
-//   .then(files => {
-//     return Promise.all(
-//       files.filter(file => file.isFile()).map(file => {
-//         // const ext = path.extname(file.name);
-//         // const name = path.basename(file.name, ext);
-//         return fsPromises.readFile(path.join(__dirname, 'components', file.name), 'utf-8');
-//       }));
-//   })
-//   .then(a => console.log(a))
-/*   .then(() => console.log(o));
-console.log(o); */
-/* fsPromises.mkdir(projectPath, {recursive: true})
-  .then(() => {
-    return fsPromises.writeFile(htmlPath, '');
-  })
-  .then(() => {
-    const stream = createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+const createReadStream = require('fs').createReadStream;
+
+const checkProjectPath = async() => {
+  try {
+    await fsPromises.access(projectPath);
+  } catch(err) {
+    await fsPromises.mkdir(projectPath);
+  }
+};
+
+const getHtml = async () => {
+  let stringHtml = await fsPromises.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+  const templateArray = stringHtml.match(/{{(\w+)}}/g);
+  const fileNamesArray = templateArray.map((template) => template.slice(2, template.length - 2));
+  const promiseArray = fileNamesArray.map(async (fileName) => {
+    const data = await fsPromises.readFile(path.join(__dirname, 'components', `${fileName}.html`), 'utf-8');
+    stringHtml = stringHtml.replace(`{{${fileName}}}`, data);
+  });
+  await Promise.all(promiseArray);
+  fsPromises.writeFile(path.join(projectPath, 'index.html'), stringHtml);
+};
+
+const copyAssets = async (dirPath = 'assets') => {
+  try {
+    await fsPromises.access(path.join(projectPath, dirPath));
+    await fsPromises.rm(path.join(projectPath, dirPath), {recursive: true});
+  } catch {() => {};} 
+  finally {
+    await fsPromises.mkdir(path.join(projectPath, dirPath));
+  }
+  const files = await fsPromises.readdir(path.join(__dirname, dirPath), {withFileTypes: true});
+  files.forEach(file => {
+    if (file.isDirectory()) {
+      copyAssets(`${dirPath}\\${file.name}`);
+    } else if (file.isFile()) {
+      fsPromises.copyFile(path.join(__dirname, dirPath, file.name), path.join(projectPath, dirPath, file.name));
+    }
+  });
+};
+
+const bundleCss = async (dirPath = 'styles') => {
+  await fsPromises.writeFile(path.join(projectPath, 'style.css'), '');
+  const cssFiles = await fsPromises.readdir(path.join(__dirname, dirPath), {withFileTypes: true});
+  cssFiles.filter(file => file.isFile() && /\.css$/i.test(file.name)).forEach(file => {
+    const stream = createReadStream(path.join(__dirname, dirPath, file.name), 'utf-8');
     let data = '';
     stream.on('data', chunk => {
       data += chunk;
     });
-    stream.on('end', () => fsPromises.appendFile(htmlPath, data));
-  })
-  .then(() => {
-    const file = createInterface(createReadStream(path.join(__dirname, 'template.html')));
-    let data = [];
-    file.on( 'line' , (line) => {
-      if (componentExp.test(line)) {
-        data.push(`${line.match(componentExp)[0].replace(/[^a-zA-z0-9]/g, '')}.html`);
-      }
-    });
-    file.on('close', () => {
-      data.forEach((it, i) => {
-        fsPromises.readFile(path.join(__dirname, 'components', it))
-          .then(component => data[i] = component);
-      });
-      console.log(data)
-    });
-  })
-  .then(data => console.log(data)); */
-fsPromises.mkdir(projectPath, {recursive: true})
-  .then(() => {
-    return fsPromises.writeFile(htmlPath, '');
-  })
-  .then(() => {
-    const file = createInterface(createReadStream(path.join(__dirname, 'template.html')));
-    file.on( 'line' , (line) => {
-      if (componentExp.test(line)) {
-        const componentName = `${line.match(componentExp)[0].replace(/[^a-zA-z0-9]/g, '')}.html`;
-        fsPromises.readFile(path.join(__dirname, 'components', componentName))
-          .then(file => fsPromises.appendFile(htmlPath, `${file}\n`));
-        const componentStream = createReadStream((path.join(__dirname, 'components', componentName)));
-        let componentData = '';
-        componentStream.on('data', chunk => {
-          componentData += chunk;
-        });
-        componentStream.on('end', () => fsPromises.appendFile(htmlPath, `${componentData}\n`));
-      } else {
-        fsPromises.appendFile(htmlPath, `${line}\n`);
-      }
-    });
+    stream.on('end', () => fsPromises.appendFile(path.join(projectPath, 'style.css'), data));
   });
+};
 
-// if (componentExp.test(line)) {
-//   const componentName = `${line.match(componentExp)[0].replace(/[^a-zA-z0-9]/g, '')}.html`;
-//   const componentStream = createReadStream((path.join(__dirname, 'components', componentName)));
-//   let componentData = '';
-//   componentStream.on('data', chunk => {
-//     componentData += chunk;
-//   });
-//   componentStream.on('end', () => fsPromises.appendFile(htmlPath, `${componentData}\n`));
-// } else {
-//   fsPromises.appendFile(htmlPath, `${line}\n`);
-// }
-
-//   fsPromises.writeFile(bundle, '')
-//   .then(() => fsPromises.readdir(stylesFolder, {withFileTypes: true}))
-//   .then(files => {
-//     files.filter(file => file.isFile() && /\.css$/i.test(file.name)).forEach(file => {
-//       const stream = createReadStream(path.join(stylesFolder, file.name), 'utf-8');
-//       let data = '';
-//       stream.on('data', chunk => {
-//         data += chunk;
-//       });
-//       stream.on('end', () => fsPromises.appendFile(bundle, data));
-//     });
-//   });
-
-
-// fsPromises.readdir(components, {withFileTypes: true})
-//   .then(files => {
-//     const names = files.filter(file => file.isFile()).map(file => {
-//       const ext = path.extname(file.name);
-//       return path.basename(file.name, ext);      
-//     });
-//     console.log(names);
-//   });
-
-
-/* fsPromises.writeFile(path.join(projectPath, 'index.html'), '')
-  .then(() => {
-    const stream = createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
-    let data = '';
-    stream.on('data', chunk => {
-      data += chunk;
-    });
-    stream.on('end', () => fsPromises.appendFile(bundle, data));
-  })
-  .then(files => {
-    files.filter(file => file.isFile() && /\.css$/i.test(file.name)).forEach(file => {
-
-      
-    });
-  });
-
-  fsPromises.readdir(path.join(__dirname, 'template.html'), {withFileTypes: true}) */
+checkProjectPath();
+getHtml();
+copyAssets();
+bundleCss();
